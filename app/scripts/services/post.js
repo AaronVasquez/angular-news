e241d65227054d6dad80a2b44bc719e37d36d204
Aaron Vasquez <aaronvasquez1@gmail.com>
2014-03-09 16:18:25 -0400

    connected firebase to angular app

--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -1,5 +1,5 @@
-'user strict';
+'use strict';
 
 app.factory('Post', function($resource){
-  return $resource("https://amber-fire-7442.firebaseio.com/posts/:id.json");
+  return $resource('https://amber-fire-7442.firebaseio.com/posts/:id.json');
 });
