0b148c200a20eed177e6286b7fb89aa9ef037604
Aaron Vasquez <aaronvasquez1@gmail.com>
2014-03-10 23:50:45 -0400

    added detail view for posts

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -5,8 +5,9 @@ app.controller('PostsCtrl', function ($scope, Post) {
   $scope.post = { url: 'http://', title: ''};
 
   $scope.submitPost = function() {
-    Post.create($scope.post).then(function() {
-      $scope.post = {url: "http://", title: ''};
+    Post.create($scope.post).then(function(respose) {
+      $location.path('/posts/' + response.name());
+      $scope.post = {url: 'http://', title: ''};
     });
   };
 
